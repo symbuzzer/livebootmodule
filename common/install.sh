53dc7045612e638015a5adbad4f4922b1b79f301
Ali Beyaz <symbuzzer@users.noreply.github.com>
2024-03-15 15:33:37 +0300

    Update install.sh

--- common/install.sh
+++ common/install.sh
@@ -8,12 +8,14 @@ ui_print "  - $output"
 ui_print "  - $width"
 ui_print "  - $height"
 
-echo "#!/system/bin/sh" > $TMPDIR/"0000bootlive"
-echo "#app_process=/system/bin/app_process64" >> $TMPDIR/"0000bootlive"
-echo "DISABLE=\"/data/adb/modules/livebootmagisk/disable\"" >> $TMPDIR/"0000bootlive"
-echo "if ! test -e \"\$DISABLE\"; then" >> $TMPDIR/"0000bootlive"
-echo "NO_ADDR_COMPAT_LAYOUT_FIXUP=1 ANDROID_ROOT=/system LD_LIBRARY_PATH=/system/lib64:/system/lib64/drm:/system/lib64/hw:/vendor/lib64:/vendor/lib64/camera:/vendor/lib64/egl:/vendor/lib64/hw:/vendor/lib64/mediacas:/vendor/lib64/mediadrm:/vendor/lib64/soundfx:/system/bin:/librootjava CLASSPATH=/data/adb/modules/livebootmagisk/liveboot /data/adb/modules/livebootmagisk/libdaemonize.so /system/bin/app_process64 /system/bin --nice-name=eu.chainfire.liveboot:root eu.chainfire.liveboot.e.d /data/adb/modules/livebootmagisk/liveboot boot dark fallbackwidth=$width fallbackheight=$height logcatlevels=WEFS logcatbuffers=C logcatformat=brief logcatnocolors dmesg=0--1 lines=80 wordwrap" >> $TMPDIR/"0000bootlive"
-echo "fi" >> $TMPDIR/"0000bootlive"
+cat > $TMPDIR/0000bootlive <<EOF
+#!/system/bin/sh
+#app_process=/system/bin/app_process64
+DISABLE="/data/adb/modules/livebootmagisk/disable"
+if ! test -e "\$DISABLE"; then
+  NO_ADDR_COMPAT_LAYOUT_FIXUP=1 ANDROID_ROOT=/system LD_LIBRARY_PATH=/system/lib64:/system/lib64/drm:/system/lib64/hw:/vendor/lib64:/vendor/lib64/camera:/vendor/lib64/egl:/vendor/lib64/hw:/vendor/lib64/mediacas:/vendor/lib64/mediadrm:/vendor/lib64/soundfx:/system/bin:/librootjava CLASSPATH=/data/adb/modules/livebootmagisk/liveboot /data/adb/modules/livebootmagisk/libdaemonize.so /system/bin/app_process64 /system/bin --nice-name=eu.chainfire.liveboot:root eu.chainfire.liveboot.e.d /data/adb/modules/livebootmagisk/liveboot boot dark fallbackwidth=$width fallbackheight=$height logcatlevels=WEFS logcatbuffers=C logcatformat=brief logcatnocolors dmesg=0--1 lines=80 wordwrap
+fi
+EOF
 
 ui_print "- Boot script created"
 
