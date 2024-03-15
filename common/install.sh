a5ae8ed8cc61635e7196f9b0570011e167355bb9
Ali Beyaz <symbuzzer@users.noreply.github.com>
2024-03-15 13:00:31 +0300

    Update install.sh

--- common/install.sh
+++ common/install.sh
@@ -1,31 +1,17 @@
-width=$(dumpsys display | grep -E "real\s+([0-9]+)x([0-9]+)" -o | awk -F 'x' '{print $1}')
-height=$(dumpsys display | grep -E "real\s+([0-9]+)x([0-9]+)" -o | awk -F 'x' '{print $2}')
+display_info=$(dumpsys display | grep -i real | grep -vi overridedisplay)
+width=$(echo "$display_info" | sed -n 's/.*real \([0-9]\+\) x \([0-9]\+\).*/\1/p')
+height=$(echo "$display_info" | sed -n 's/.*real \([0-9]\+\) x \([0-9]\+\).*/\2/p')
 
 ui_print "- Getting screen size"
 ui_print "  - $width"
 ui_print "  - $height"
 
-echo "#!/system/bin/sh
-#app_process=/system/bin/app_process64
-DISABLE=\"/data/adb/modules/livebootmagisk/disable\"
-if ! test -e \"\$DISABLE\"; then
-  NO_ADDR_COMPAT_LAYOUT_FIXUP=1
-  ANDROID_ROOT=/system
-  LD_LIBRARY_PATH=/system/lib64:/system/lib64/drm:/system/lib64/hw:/vendor/lib64:/vendor/lib64/camera:/vendor/lib64/egl:/vendor/lib64/hw:/vendor/lib64/mediacas:/vendor/lib64/mediadrm:/vendor/lib64/soundfx:/system/bin:/librootjava
-  CLASSPATH=/data/adb/modules/livebootmagisk/liveboot
-  /data/adb/modules/livebootmagisk/libdaemonize.so
-  /system/bin/app_process64
-  /system/bin --nice-name=eu.chainfire.liveboot:root eu.chainfire.liveboot.e.d /data/adb/modules/livebootmagisk/liveboot boot dark
-  fallbackwidth=$width
-  fallbackheight=$height
-  logcatlevels=WEFS
-  logcatbuffers=C
-  logcatformat=brief
-  logcatnocolors
-  dmesg=0--1
-  lines=80
-  wordwrap
-fi" > $MODPATH/0000bootlive
+echo "#!/system/bin/sh" > "$MODPATH/0000bootlive"
+echo "#app_process=/system/bin/app_process64" >> "$MODPATH/0000bootlive"
+echo "DISABLE=\"/data/adb/modules/livebootmagisk/disable\"" >> "$MODPATH/0000bootlive"
+echo "if ! test -e \"\$DISABLE\"; then" >> "$MODPATH/0000bootlive"
+echo "NO_ADDR_COMPAT_LAYOUT_FIXUP=1 ANDROID_ROOT=/system LD_LIBRARY_PATH=/system/lib64:/system/lib64/drm:/system/lib64/hw:/vendor/lib64:/vendor/lib64/camera:/vendor/lib64/egl:/vendor/lib64/hw:/vendor/lib64/mediacas:/vendor/lib64/mediadrm:/vendor/lib64/soundfx:/system/bin:/librootjava CLASSPATH=/data/adb/modules/livebootmagisk/liveboot /data/adb/modules/livebootmagisk/libdaemonize.so /system/bin/app_process64 /system/bin --nice-name=eu.chainfire.liveboot:root eu.chainfire.liveboot.e.d /data/adb/modules/livebootmagisk/liveboot boot dark fallbackwidth=$width fallbackheight=$height logcatlevels=WEFS logcatbuffers=C logcatformat=brief logcatnocolors dmesg=0--1 lines=80 wordwrap" >> "$MODPATH/0000bootlive"
+echo "fi" >> "$MODPATH/0000bootlive"
 
 ui_print "- Boot script created"
 
