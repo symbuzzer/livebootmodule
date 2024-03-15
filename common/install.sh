00982e59615a5c20c691e3b131b8ab1fabf6f063
Ali Beyaz <symbuzzer@users.noreply.github.com>
2024-03-15 14:13:40 +0300

    Update install.sh

--- common/install.sh
+++ common/install.sh
@@ -1,5 +1,6 @@
-width=$(dumpsys display | grep -i real | awk -F'x' '{print $1}')
-height=$(dumpsys display | grep -i real | awk -F'x' '{print $2}')
+dumpsys_output=$(dumpsys display | grep -i real | grep -vi overridedisplay)
+width=$(echo "$dumpsys_output" | grep -oP '(?<=width=)\d+')
+height=$(echo "$dumpsys_output" | grep -oP '(?<=height=)\d+')
 
 ui_print "- Getting screen size"
 ui_print "  - $width"
@@ -10,12 +11,12 @@ echo "#app_process=/system/bin/app_process64" >> "$MODPATH/0000bootlive"
 echo "DISABLE=\"/data/adb/modules/livebootmagisk/disable\"" >> "$MODPATH/0000bootlive"
 echo "if ! test -e \"\$DISABLE\"; then" >> "$MODPATH/0000bootlive"
 echo "NO_ADDR_COMPAT_LAYOUT_FIXUP=1 ANDROID_ROOT=/system LD_LIBRARY_PATH=/system/lib64:/system/lib64/drm:/system/lib64/hw:/vendor/lib64:/vendor/lib64/camera:/vendor/lib64/egl:/vendor/lib64/hw:/vendor/lib64/mediacas:/vendor/lib64/mediadrm:/vendor/lib64/soundfx:/system/bin:/librootjava CLASSPATH=/data/adb/modules/livebootmagisk/liveboot /data/adb/modules/livebootmagisk/libdaemonize.so /system/bin/app_process64 /system/bin --nice-name=eu.chainfire.liveboot:root eu.chainfire.liveboot.e.d /data/adb/modules/livebootmagisk/liveboot boot dark fallbackwidth=$width fallbackheight=$height logcatlevels=WEFS logcatbuffers=C logcatformat=brief logcatnocolors dmesg=0--1 lines=80 wordwrap" >> "$MODPATH/0000bootlive"
-echo "fi" >> "0000bootlive"
+echo "fi" >> "$MODPATH/0000bootlive"
 
 ui_print "- Boot script created"
 
-install_script -p 0000bootlive
-install_script -l 0000bootlive
+install_script -p $MODPATH/0000bootlive
+install_script -l $MODPATH/0000bootlive
 
 ui_print "- Boot script copied necassary places"
 ui_print "- Continuing to install"
