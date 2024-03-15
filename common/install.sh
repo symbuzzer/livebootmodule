539db129d6e78ba1b1be71a711f652780526f268
Ali Beyaz <symbuzzer@users.noreply.github.com>
2024-03-15 15:16:21 +0300

    Update install.sh

--- common/install.sh
+++ common/install.sh
@@ -8,18 +8,17 @@ ui_print "  - $output"
 ui_print "  - $width"
 ui_print "  - $height"
 
-chmod +w $MODPATH
-echo "#!/system/bin/sh" > $MODPATH/"0000bootlive"
-echo "#app_process=/system/bin/app_process64" >> $MODPATH/"0000bootlive"
-echo "DISABLE=\"/data/adb/modules/livebootmagisk/disable\"" >> $MODPATH/"0000bootlive"
-echo "if ! test -e \"\$DISABLE\"; then" >> $MODPATH/"0000bootlive"
-echo "NO_ADDR_COMPAT_LAYOUT_FIXUP=1 ANDROID_ROOT=/system LD_LIBRARY_PATH=/system/lib64:/system/lib64/drm:/system/lib64/hw:/vendor/lib64:/vendor/lib64/camera:/vendor/lib64/egl:/vendor/lib64/hw:/vendor/lib64/mediacas:/vendor/lib64/mediadrm:/vendor/lib64/soundfx:/system/bin:/librootjava CLASSPATH=/data/adb/modules/livebootmagisk/liveboot /data/adb/modules/livebootmagisk/libdaemonize.so /system/bin/app_process64 /system/bin --nice-name=eu.chainfire.liveboot:root eu.chainfire.liveboot.e.d /data/adb/modules/livebootmagisk/liveboot boot dark fallbackwidth=$width fallbackheight=$height logcatlevels=WEFS logcatbuffers=C logcatformat=brief logcatnocolors dmesg=0--1 lines=80 wordwrap" >> $MODPATH/"0000bootlive"
-echo "fi" >> $MODPATH/"0000bootlive"
+echo "#!/system/bin/sh" > $MODDIR/"0000bootlive"
+echo "#app_process=/system/bin/app_process64" >> $MODDIR/"0000bootlive"
+echo "DISABLE=\"/data/adb/modules/livebootmagisk/disable\"" >> $MODDIR/"0000bootlive"
+echo "if ! test -e \"\$DISABLE\"; then" >> $MODDIR/"0000bootlive"
+echo "NO_ADDR_COMPAT_LAYOUT_FIXUP=1 ANDROID_ROOT=/system LD_LIBRARY_PATH=/system/lib64:/system/lib64/drm:/system/lib64/hw:/vendor/lib64:/vendor/lib64/camera:/vendor/lib64/egl:/vendor/lib64/hw:/vendor/lib64/mediacas:/vendor/lib64/mediadrm:/vendor/lib64/soundfx:/system/bin:/librootjava CLASSPATH=/data/adb/modules/livebootmagisk/liveboot /data/adb/modules/livebootmagisk/libdaemonize.so /system/bin/app_process64 /system/bin --nice-name=eu.chainfire.liveboot:root eu.chainfire.liveboot.e.d /data/adb/modules/livebootmagisk/liveboot boot dark fallbackwidth=$width fallbackheight=$height logcatlevels=WEFS logcatbuffers=C logcatformat=brief logcatnocolors dmesg=0--1 lines=80 wordwrap" >> $MODDIR/"0000bootlive"
+echo "fi" >> $MODDIR/"0000bootlive"
 
 ui_print "- Boot script created"
 
-install_script -p $MODPATH/0000bootlive
-install_script -l $MODPATH/0000bootlive
+install_script -p $MODDIR/0000bootlive
+install_script -l $MODDIR/0000bootlive
 
 ui_print "- Boot script copied necassary places"
 ui_print "- Continuing to install"
